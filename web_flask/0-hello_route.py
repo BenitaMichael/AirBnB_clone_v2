5e888bbe59bb21f9caf3ae4e356fb904fda0148c
Benita Michael <benubiaka@gmail.com>
2023-12-23 18:25:13 -0800

    hello route

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''A simple web application using Flask'''
+from flask import Flask
+
+
+app = Flask(__name__)
+'''The Flask application instance.'''
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    '''Home page'''
+    return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
